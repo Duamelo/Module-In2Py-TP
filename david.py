1ea73860079e9a023c80cc3c2050c26d05d470e8
Duamelo <44211303+Duamelo@users.noreply.github.com>
2021-06-17 16:01:01 +0200

    Add files via upload

---
+++ david.py
@@ -0,0 +1,6 @@
+david = {'python':[17,18,19], 'electro': [14,15,16], 'langage C': [16, 18, 19]}
+moyPy = (david['python'][0]+ david['python'][1]+ david['python'][2])/3
+moyElec = (david['electro'][0]+ david['electro'][1]+ david['electro'][2])/3
+moylangC = (david['langage C'][0]+ david['langage C'][1]+ david['langage C'][2])/3
+moyenne = (moyPy + moyElec + moylangC)/3
+print(moyenne)
